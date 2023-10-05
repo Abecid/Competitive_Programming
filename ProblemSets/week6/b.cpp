100a32b411a98796a04e14d5d739be4db215c6e8
Adam <cmlee3211@gmail.com>
2023-10-05 03:04:29 -0700

    Fix w6b

--- ProblemSets/week6/b.cpp
+++ ProblemSets/week6/b.cpp
@@ -3,19 +3,21 @@
 #include <queue>
 using namespace std;
 
-bool bfs(int start_x, int start_y, vector<vector<char>>& maze) {
+int bfs_exit(int start_x, int start_y, vector<vector<char>>& maze, vector<vector<bool>>& visited) {
     int n = maze.size(), m = maze[0].size();
-    vector<vector<bool>> visited(n, vector<bool>(m, false));
     
     queue<pair<int, int>> q;
     q.push({start_x, start_y});
     visited[start_x][start_y] = true;
 
+    int total_gs = 0;
+
     while(!q.empty()) {
         int x = q.front().first, y = q.front().second;
         q.pop();
 
-        if(x == n-1 && y == m-1) return true;
+        if(maze[x][y] == 'B') return -1;
+        if(maze[x][y] == 'G') total_gs++;
 
         int dx[] = {0, 0, 1, -1};
         int dy[] = {1, -1, 0, 0};
@@ -27,7 +29,7 @@ bool bfs(int start_x, int start_y, vector<vector<char>>& maze) {
             }
         }
     }
-    return false;
+    return total_gs;
 }
 
 int main() {
@@ -35,8 +37,11 @@ int main() {
     cin >> t;
     while(t--) {
         int n, m;
+        int gs = 0;
+        int num_gs;
         cin >> n >> m;
         vector<vector<char>> maze(n, vector<char>(m));
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
         
         for(int i = 0; i < n; i++)
             for(int j = 0; j < m; j++)
@@ -59,22 +64,30 @@ int main() {
                         }
                     }
                 }
+                if(maze[i][j] == 'G') gs++;
             }
         }
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(maze[i][j] == 'G') {
-                    if(!bfs(i, j, maze)) {
-                        cout << "No" << endl;
-                        goto next_testcase;
-                    }
-                }
-            }
+        if(gs == 0) {
+            cout << "Yes" << endl;
+            goto next_testcase;
         }
-        cout << "Yes" << endl;
 
-        next_testcase: ; // empty label
+        if(maze[n-1][m-1] == '#') {
+            cout << "No" << endl;
+            goto next_testcase;
+        }
+
+        num_gs = bfs_exit(n-1, m-1, maze, visited);
+        
+        if(num_gs == -1)
+            cout << "No" << endl;
+        else if(num_gs == gs)
+            cout << "Yes" << endl;
+        else 
+            cout << "No" << endl;
+
+        next_testcase: ;
     }
     return 0;
 }
